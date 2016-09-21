0ce1ff1986ebfad0134c231f643dda92df231a78
Austin Lanari & Jenn Su & per ohrstrom <pair+foggy1.jennysoup.perohrstrom@devbootcamp.com>
2016-09-21 12:59:53 -0400

    Turn off voteable validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,9 +3,9 @@ class Vote < ActiveRecord::Base
   belongs_to :voter, class_name: "User"
 
   validates :voter_id, presence: true
-  validates :voteable_id, presence: true
-  validates :voteable_type, presence: true
-  
+  # validates :voteable_id, presence: true
+  # validates :voteable_type, presence: true
+
   validates :voter_id, uniqueness: { scope: [:voteable_id, :voteable_type] }
 
 end
