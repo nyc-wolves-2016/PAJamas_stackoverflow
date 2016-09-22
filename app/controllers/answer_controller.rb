b3ad1082e079440cefcdebcfa98a4bbbbcfb3c25
Austin Lanari <email": null>
2016-09-22 14:27:17 -0400

    Make comments deletable

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,7 +1,7 @@
 post '/answers/:answer_id/comments/new' do
 
   @answer = Answer.find_by(id: params[:answer_id])
-  @question = @answer.question
+  @answer = @answer.question
   @comment = Comment.new(params[:comment])
   if @comment.save
     @answer.comments << @comment
@@ -11,4 +11,12 @@ post '/answers/:answer_id/comments/new' do
     @errors = @comment.errors.full_message
     erb :'questions/show'
   end
+end
+
+delete '/answers/:answer_id/comments/:id' do
+  @answer = Answer.find_by(id: params[:answer_id])
+  @question = @answer.question
+  @comment = Comment.find_by(id: params[:id])
+  @comment.destroy
+  redirect "/questions/#{@question.id}"
 end
--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -139,7 +139,9 @@ end
 
 delete '/questions/:question_id/comments/:id' do
   @question = Question.find_by(id: params[:question_id])
-  @comment = Comment.find_by(id: )
+  @comment = Comment.find_by(id: params[:id])
+  @comment.destroy
+  redirect "/questions/#{@question.id}"
 end
 
 
