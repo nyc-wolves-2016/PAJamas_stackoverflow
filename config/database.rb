95bfb808059e3b55e8711da8f8d5a398c20242e7
Austin Lanari <austin.lanari@gmail.com>
2016-09-22 12:12:58 -0400

    not break things

--- config/database.rb
+++ config/database.rb
@@ -33,6 +33,7 @@ DB_NAME = db.path[1..-1]
 
 ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
   :port     => db.port,
   :username => db.user,
   :password => db.password,
